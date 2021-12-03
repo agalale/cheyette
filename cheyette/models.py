1. Correct mean and standard deviation for x[t], y[t] for the Vasicek process. 2. Zero coupon bond and swaption pricing using approximation in the annuity measure. 3. Pricing swaptions using Jamshidian decomposition.

cheyette/models.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 import numpy as np
 from scipy.stats import norm
 from cheyette.processes import CheyetteProcess, VasicekProcess
-from cheyette.products import CheyetteProduct, ZCBCall
+from cheyette.products import CheyetteProduct, BondOpt, PayerSwaption, OptionType
 from cheyette.curves import Curve
 from cheyette.boundary_conditions import CheyetteBC
 from cheyette.discretization import Mesh2D, CheyetteStepping, CheyetteOperator
@@ -79,32 +79,68 @@ class CheyettePDEModel(CheyetteModel):
 
         return self
 
+
 class CheyetteAnalyticModel(CheyetteModel):
     """
     Better reimplement using visitor pattern
     """
     def price(self, curve: Curve, process: CheyetteProcess, product: CheyetteProduct, valuation_time: float):
         results = dict()
-        if isinstance(product, ZCBCall):
-            if isinstance(process, VasicekProcess):
-                k = process.mean_rev
-                s = process.local_vol
-                S = product.expiry
-                T = product.bond_expiry
-                K = product.strike
-                B_T = curve.df(T)
-                B_S = curve.df(S)
-                nu = s ** 2 / (2 * k ** 3) * (1 - np.exp(-k * (T - S))) ** 2 * (1 - np.exp(-2 * k * S))
-                d_plus = (np.log(B_T / (K * B_S)) + 0.5 * nu) / np.sqrt(nu)
-                d_minus = d_plus - np.sqrt(nu)
+        if isinstance(product, BondOpt):
+            assert isinstance(process, VasicekProcess), f'Analytic model can not be used to price bond option when the underlying process is {process.__class__}'
+            k = process.mean_rev
+            s = process.local_vol
+            S = product.expiry
+            T = product.bond_expiry
+            K = product.strike
+            B_T = curve.df(T)
+            B_S = curve.df(S)
+            nu = s ** 2 / (2 * k ** 3) * (1 - np.exp(-k * (T - S))) ** 2 * (1 - np.exp(-2 * k * S))
+            d_plus = (np.log(B_T / (K * B_S)) + 0.5 * nu) / np.sqrt(nu)
+            d_minus = d_plus - np.sqrt(nu)
+            if product.option_type == OptionType.call:
                 results['PV'] = B_T * norm.cdf(d_plus) - K * B_S * norm.cdf(d_minus)
+            elif product.option_type == OptionType.put:
+                results['PV'] = K * B_S * norm.cdf(-d_minus) - B_T * norm.cdf(-d_plus)
 
-            else:
-                raise Exception(f'Analytic engine can\'t be used to price ZCB when '
-                                + f'the underlying process is {process.__class__}')
+        if isinstance(product, PayerSwaption):
+            """
+            Jamshidian decomposition
+            """
+            T_first = product.underlying_times[0]
+            T_last = product.underlying_times[-1]
+            Ts = product.underlying_times
+            taus = [T2 - T1 for T1, T2 in zip(Ts, Ts[1:])]
+            K = product.strike
+
+            def objective(r):
+                return 1 - np.exp(-r*(T_last - T_first)) - K\
+                       * sum((T2 - T1) * np.exp(-r*(T2 - T_first)) for T1, T2 in zip(Ts, Ts[1:]))
+
+            right_r = 0.01
+            while objective(right_r) < 0:
+                right_r *= 2
+
+            left_r = right_r / 2
+
+            while True:
+                mid_r = (left_r + right_r) / 2
+                if objective(mid_r) < 0:
+                    left_r = mid_r
+                else:
+                    right_r = mid_r
+                if right_r - left_r < 1e-6:
+                    break
+
+            strikes = [np.exp(-mid_r*(T - T_first)) for T in Ts[1:]]
+            bond_opts = [BondOpt(K, product.expiry, T, OptionType.put) for K, T in zip(strikes, Ts[1:])]
+            bond_opts_pvs = [self.price(curve, process, p, valuation_time)['PV'] for p in bond_opts]
+
+            results['PV'] = bond_opts_pvs[-1] + sum(K * tau * pv for tau, pv in zip(taus, bond_opts_pvs))
+            results['Method'] = 'Jamshidian decomposition'
 
         else:
-            raise Exception(f'Product {product.__class__} is not supported by the analytic engine')
+            Exception(f'Product {product.__class__} is not supported by the analytic model')
 
         return results
 

cheyette/processes.py
@@ -87,13 +87,13 @@ class VasicekProcess(ConstMeanRevProcess):
         return self.local_vol
 
     def x_mean(self, t: float):
-        return 0.5 * self.local_vol**2 * t**2
+        return self.local_vol ** 2 / (2 * self.mean_rev ** 2) * (1 - np.exp(-self.mean_rev * t)) ** 2
 
     def y_mean(self, t: float):
-        return self.local_vol ** 2 * t
+        return self.local_vol ** 2 / (2 * self.mean_rev) * (1 - np.exp(-2 * self.mean_rev * t))
 
     def x_stddev(self, t: float):
-        return self.local_vol * np.sqrt(t)
+        return np.sqrt(self.local_vol ** 2 / (2 * self.mean_rev) * (1 - np.exp(-2 * self.mean_rev * t)))
 
     def y_stddev(self, t: float):
         return 0.0
@@ -103,7 +103,8 @@ class VasicekProcess(ConstMeanRevProcess):
         return curve.fwd(0, t) + x
 
     def __repr__(self):
-        return f'dx[t] = (y[t] - kappa*x[t])dt + sigma*dW[t]\n' \
+        return f'Stationary Hull-White process' \
+               f'dx[t] = (y[t] - kappa*x[t])dt + sigma*dW[t]\n' \
                f'dy[t] = (sigma**2 - 2*kappa*y[t])dt\n' \
                f'kappa={self.mean_rev}, sigma={self.local_vol}'
 
@@ -115,6 +116,47 @@ class VasicekProcess(ConstMeanRevProcess):
             self.mean_rev = value
             return self
         raise Exception(f'Attribute {key} does not exist for {self.__class__}')
+
+
+class QuadraticProcess(ConstMeanRevProcess):
+        """
+            dx[t] = (y[t] - mean_rev * y[t])*dt + sigma(x[t]) * dW[t]
+            dy[t] = (sigma(x[t])**2 - 2 * mean-rev * y[t]) * dt
+            sigma(x) = a + b * x + c * x**2
+        """
+
+        def __init__(self, mean_rev: float, a: float, b: float, c: float):
+            ConstMeanRevProcess.__init__(self, mean_rev)
+            self.a = a
+            self.b = b
+            self.c = c
+
+        def mu_x(self, t: float, x: float, y: float) -> float:
+            return y - self.mean_rev * x
+
+        def gamma_x(self, t: float, x: float, y: float) -> float:
+            return self.a + self.b * x + self.c * x**2
+
+        def x_mean(self, t: float):
+            return self.a ** 2 / (2 * self.mean_rev ** 2) * (1 - np.exp(-self.mean_rev * t))**2
+
+        def y_mean(self, t: float):
+            return self.a**2 / (2 * self.mean_rev) * (1 - np.exp(-2 * self.mean_rev * t))
+
+        def x_stddev(self, t: float):
+            return np.sqrt(self.a**2 / (2 * self.mean_rev) * (1 - np.exp(-2 * self.mean_rev * t)))
+
+        def y_stddev(self, t: float):
+            return 0.0
+
+        @classmethod
+        def r(cls, curve: Curve, t: float, x: float):
+            return curve.fwd(0, t) + x
+
+        def __repr__(self):
+            return f'dx[t] = (y[t] - kappa*x[t])dt + sigma(x[t])*dW[t]\n' \
+                   f'dy[t] = (sigma(x[t])**2 - 2*kappa*y[t])dt\n' \
+                   f'sigma(x)=a + b * x + c * x**2'
 
 
 class QuadraticAnnuityProcess(ConstMeanRevProcess):
@@ -140,14 +182,19 @@ class QuadraticAnnuityProcess(ConstMeanRevProcess):
         return 0.0
 
     def x_mean(self, t: float):
-        return 0.5 * self.a**2 * t**2
+        return self.a ** 2 / (2 * self.mean_rev ** 2) * (1 - np.exp(-self.mean_rev * t)) ** 2
 
     def y_mean(self, t: float):
-        return self.a ** 2 * t
+        return self.a ** 2 / (2 * self.mean_rev) * (1 - np.exp(-2 * self.mean_rev * t))
 
     def x_stddev(self, t: float):
-        return self.a * np.sqrt(t)
+        return np.sqrt(self.a ** 2 / (2 * self.mean_rev) * (1 - np.exp(-2 * self.mean_rev * t)))
 
     def y_stddev(self, t: float):
         return 0.0
 
+    def __repr__(self):
+        return 'Quadratic annuity process\n' \
+               'dx[t]=-k*x[t]*dt + sigma(x[t])*dW[t]\n' \
+               'dy[t]=(sigma(x[t])**2 - 2*k*x[t])*dt\n' \
+               f'sigma(x)={self.a:.2f} + {self.b:.2f} * x + {self.c:.2f} * x**2'

cheyette/products.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 import numpy as np
 from cheyette.processes import CheyetteProcess, ConstMeanRevProcess
 from cheyette.curves import Curve
+from enum import Enum
 
 
 class CheyetteProduct(ABC):
@@ -24,20 +25,31 @@ class ZCB(CheyetteProduct):
         return f'ZCB(expiry={self.expiry:.2f})'
 
 
-class ZCBCall(CheyetteProduct):
-    def __init__(self, strike: float, expiry: float, bond_expiry: float) -> None:
+class OptionType(Enum):
+    call = 0
+    put = 1
+
+
+class BondOpt(CheyetteProduct):
+    def __init__(self, strike: float, expiry: float, bond_expiry: float, option_type: OptionType) -> None:
         CheyetteProduct.__init__(self, expiry)
         self.strike = strike
         self.bond_expiry = bond_expiry
+        self.option_type = option_type
 
     def intrinsic_value(self, curve: Curve, process: CheyetteProcess, t: float, x: float, y: float):
-        return max(process.df(curve, t, self.bond_expiry, x, y)
-                   - process.df(curve, t, self.expiry, x, y) * self.strike, 0.0)
+        if self.option_type == OptionType.call:
+            return max(process.df(curve, t, self.bond_expiry, x, y)
+                       - process.df(curve, t, self.expiry, x, y) * self.strike, 0.0)
+        if self.option_type == OptionType.put:
+            return max(process.df(curve, t, self.expiry, x, y) * self.strike
+                       - process.df(curve, t, self.bond_expiry, x, y), 0.0)
 
     def __repr__(self):
-        return f'ZCBCall(strike={self.strike}, ' \
+        return f'BondOpt(strike={self.strike}, ' \
                f'expiry={self.expiry}, ' \
-               f'bond_expiry={self.bond_expiry})'
+               f'bond_expiry={self.bond_expiry}, )'\
+               f'option_type={self.option_type})'
 
 
 class PayerSwaption(CheyetteProduct):
@@ -83,3 +95,33 @@ class PayerSwaptionAnnuity(CheyetteProduct):
 
     def __repr__(self):
         return f'PayerSwaptionAnnuity(strike={self.strike:.2f}, expiry={self.expiry:.2f})'
+
+
+class ZCBCallAnnuity(CheyetteProduct):
+    def __init__(self, strike: float, expiry: float, bond_expiry: float, curve: Curve, process: ConstMeanRevProcess):
+        CheyetteProduct.__init__(self, expiry)
+        self.strike = strike
+        self.expiry = expiry
+        self.bond_expiry = bond_expiry
+
+        self.underlying_times = [expiry, bond_expiry]
+        self.annuity = curve.annuity(self.underlying_times)
+        ws = [(t2 - t1) * curve.df(t2) / self.annuity for t1, t2 in zip(self.underlying_times, self.underlying_times[1:])]
+        psi_factors_first = [process.G(self.underlying_times[0], T) * np.exp(-process.mean_rev * self.underlying_times[0])
+                             for T in self.underlying_times[1:]]
+        psi_factors_last = [process.G(T, self.underlying_times[-1]) * np.exp(-process.mean_rev * T) for T in
+                            self.underlying_times[1:]]
+        self.alpha_factor_first = sum(w * psi for w, psi in zip(ws, psi_factors_first))
+        self.alpha_factor_last = sum(w * psi for w, psi in zip(ws, psi_factors_last))
+        self.df_first = curve.df(self.underlying_times[0]) / self.annuity
+        self.df_last = curve.df(self.underlying_times[-1]) / self.annuity
+
+    def intrinsic_value(self, curve: Curve, process: ConstMeanRevProcess, t: float, x: float, y: float) -> float:
+        multiplier = np.exp(process.mean_rev * t)
+        alpha_first = self.alpha_factor_first * multiplier
+        alpha_last = self.alpha_factor_last * multiplier
+        return self.annuity * max(self.df_last * np.exp(-alpha_last * x - 0.5 * alpha_last ** 2 * y)
+                                  - self.strike * self.df_first * np.exp(alpha_first * x - 0.5 * alpha_first ** 2 * y), 0.0)
+
+    def __repr__(self):
+        return f'ZCBCallAnnuity(strike={self.strike}, expiry={self.expiry}, bond_expiry={self.bond_expiry})'